c03fad779c4e4e7bc1d0538aa81070dd44c8e456
Harsha1309 <harshyadav1393@gmail.com>
2026-06-18 23:11:58 +0530

    Updated the backend to S3 and DynamoDB

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,21 @@ variable "bucket_name" {
   type        = string
   default     = "my-unique-starter-bucket-123452323284" # Change this to something unique
 }
+
+variable "tf_state_bucket" {
+  description = "The S3 bucket name for storing Terraform state"
+  type        = string
+  default     = "terraform-state-bucket-unique-name" # Change this to something unique
+}
+
+variable "tf_state_key" {
+  description = "The S3 key path for the Terraform state file"
+  type        = string
+  default     = "prod/terraform.tfstate"
+}
+
+variable "tf_lock_table" {
+  description = "The DynamoDB table name for Terraform state locking"
+  type        = string
+  default     = "terraform-lock-table"
+}
